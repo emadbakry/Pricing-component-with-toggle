cf3c64c8879e4782e327a663d08ddd4fa8b7d902
emadbakry <101658737+emadbakry@users.noreply.github.com>
2022-11-27 22:56:12 +0300

    first-sub
    this isthe first submittion

---
+++ index.js
@@ -0,0 +1,25 @@
+let togglePrice_btn = document.querySelector(".togglePrice__btn");
+let prices = document.querySelectorAll(".box .price");
+let m_prices = [19.99, 24.99, 39.99];
+let year_prices = [199.99, 249.99, 399.99];
+
+let updatePrices = (arr) => {
+        
+    for (i in prices) {    
+        prices[i].textContent = arr[i];
+    }
+}
+
+togglePrice_btn.addEventListener('click', () => toggle());
+let toggle = () => {
+    togglePrice_btn.toggleAttribute('left_placed');
+    togglePrice_btn.hasAttribute("left_placed")
+        ? updatePrices(year_prices)
+        : updatePrices(m_prices);
+    
+   
+    
+        
+    
+};
+
